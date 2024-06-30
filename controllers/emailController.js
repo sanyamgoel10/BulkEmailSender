4a60b3c757cb02d3ac7392aada5378c89568cbf5
Sanyam Goel <sanyamgoel10@gmail.com>
2024-06-30 16:46:14 +0530

    changes in controller structure

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -6,59 +6,58 @@ if (typeof config.senderEmail != 'string' || typeof config.senderPassword != 'st
     process.exit(1);
 }
 
-exports.sendEmails = async (req, res) => {
+exports.sendMultipleEmails = async (req, res) => {
     try {
         if (typeof req.body.TemplateId == 'undefined') {
             return res.status(400).send("Invalid TemplateId");
-        }
+        } 
         if (typeof req.body.ReceiverDetails != 'object') {
             return res.status(400).send("Invalid ReceiverDetails");
         }
+
+        const templateId = req.body.TemplateId;
         const userList = req.body.ReceiverDetails;
-        if('undefined' == typeof userList.name || 'undefined' == typeof userList.email || !Array.isArray(userList.name) || !Array.isArray(userList.email)){
-            return res.status(400).send("Invalid name, email in ReceiverDetails");
+
+        if('undefined' == typeof userList.email || !Array.isArray(userList.email)){
+            return res.status(400).send("Invalid email in ReceiverDetails");
         }
-        if(userList.name.length != userList.email.length){
-            return res.status(400).send("Arrays length do not match");
+
+        for(let elem of Object.keys(userList)){
+            if(!Array.isArray(userList[elem]) || userList[elem].length != userList['email'].length){
+                return res.status(400).send("Arrays length do not match in ReceiverDetails");
+            }
         }
         
         let emailSent = [], emailNotSent = [];
 
-        for(let i=0; i<userList.name.length; i++){
-            let currName = userList.name[i];
-            let currEmail = userList.email[i];
-
-            if('string' == typeof currEmail){
-                let reqObjArr = {
-                    name: currName,
-                    email: currEmail
-                };
-                Object.keys(userList).forEach((elem) => {
-                    if(elem != 'name' && elem != 'email'){
-                        if(!Array.isArray(userList[elem]) || userList[elem].length != userList.name.length){
-                            return res.status(400).send("Arrays length do not match");
-                        }
-                        reqObjArr[elem] = userList[elem][i];
+        if(config.emailTemplate[templateId] && config.emailTemplate[templateId].subject && config.emailTemplate[templateId].body){
+            const emailSubject = config.emailTemplate[templateId].subject;
+            const emailBody = config.emailTemplate[templateId].body;
+            for(let i=0; i<userList.email.length; i++){
+                const currEmail = userList.email[i];
+                if('string' == typeof currEmail && (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/).test(currEmail)){
+                    let currSubject = emailSubject, currBody = emailBody;
+                    for(let elem of Object.keys(userList)){
+                        currSubject = currSubject.replaceAll('[['+elem+']]', userList[elem][i]);
+                        currBody = currBody.replaceAll('[['+elem+']]', userList[elem][i]);
                     }
-                });
-                if(!(await sendEmail(reqObjArr, req.body.TemplateId))){
-                    emailNotSent.push({Email: currEmail, Name: currName});
+                    if(await sendEmail(currEmail, currSubject, currBody)){
+                        console.log("Email sent to : ", currEmail);
+                        emailSent.push(currEmail);
+                    }else{
+                        emailNotSent.push(currEmail);
+                    }
                 }else{
-                    console.log('Email sent to:', currName, 'on EmailId:', currEmail);
-                    emailSent.push({Email: currEmail, Name: currName});
-                }                
-            }else{
-                emailNotSent.push({Email: currEmail, Name: currName});
+                    emailNotSent.push(currEmail);
+                }
             }
+            return res.send({Success: emailSent, Failed: emailNotSent});
+        }else{
+            return res.status(400).send("TemplateId not found.");
         }
-
-        res.send({
-            Success: emailSent,
-            Failed: emailNotSent
-        });
     } catch (error) {
         console.error(error);
-        res.status(500).send("Failed to send email.");
+        return res.status(500).send("Failed to send email.");
     }
 };
 
@@ -75,22 +74,18 @@ const transporter = nodemailer.createTransport({
 });
 
 // Function to send an email
-const sendEmail = async (receiverDetails, templateId) => {
-    if(config.emailTemplate[templateId] && config.emailTemplate[templateId].subject && config.emailTemplate[templateId].body){
-        let emailSubject = config.emailTemplate[templateId].subject;
-        let emailBody = config.emailTemplate[templateId].body;
-        Object.keys(receiverDetails).forEach((elem) => {
-            emailSubject.replaceAll('[['+elem+']]', receiverDetails.elem);
-            emailBody.replaceAll('[['+elem+']]', receiverDetails.elem);
-        })
-        const mailOptions = {
-            from: config.senderEmail,
-            to: receiverDetails.email,
-            subject: emailSubject,
-            text: emailBody
-        };
-        return await transporter.sendMail(mailOptions);
+const sendEmail = async (receiverEmail, emailSubject, emailBody) => {
+    const mailOptions = {
+        from: config.senderEmail,
+        to: receiverEmail,
+        subject: emailSubject,
+        text: emailBody
+    };
+    try {
+        await transporter.sendMail(mailOptions);
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
     }
-    console.log('TemplateId not found');
-    return false; 
 };
--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -3,6 +3,6 @@ const emailController = require('../controllers/emailController');
 
 const router = express.Router();
 
-router.post('/sendemail', emailController.sendEmails);
+router.post('/sendemail', emailController.sendMultipleEmails);
 
 module.exports = router;
