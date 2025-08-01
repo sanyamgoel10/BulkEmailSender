adafc60bab477bafd644cb0dbb363436ccc5807b
sanyamgoel <sanyam.goel@octrotalk.com>
2025-08-01 11:46:27 +0530

    kafka change

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -106,12 +106,60 @@ class EmailController {
                     msg: 'Invalid body'
                 });
             }
-            
-            await KafkaService.sendMessage('send-email-topic', JSON.stringify(req.body));
-            
-            return res.status(200).json({
-                status: 1,
-                msg: 'Send email packet sent to kafka'
+
+            if (!UtilService.checkValidString(req.body.TemplateId)) {
+                return res.status(400).json({
+                    status: 0,
+                    msg: 'Invalid TemplateId'
+                });
+            }
+
+            let finalKafkaEvents = [];
+            if (UtilService.checkValidArray(req.body.ReceiverDetails) && (req.body.ReceiverDetails).length > 0) {
+                for (let i = 0; i < (req.body.ReceiverDetails).length; i++) {
+                    if (!UtilService.checkValidObject(req.body.ReceiverDetails[i])) {
+                        return res.status(400).json({
+                            status: 0,
+                            msg: `Invalid ReceiverDetails json at index ${i}`
+                        });
+                    }
+                    if (!UtilService.checkValidEmailId(req.body.ReceiverDetails[i].email)) {
+                        return res.status(400).json({
+                            status: 0,
+                            msg: `Invalid email in ReceiverDetails at index ${i}`
+                        });
+                    }
+                    let currEvent = {
+                        TemplateId: req.body.TemplateId
+                    };
+                    for (let elem of Object.keys(req.body.ReceiverDetails[i])) {
+                        currEvent[elem] = req.body.ReceiverDetails[i][elem];
+                    }
+                    finalKafkaEvents.push(currEvent);
+                }
+            } else {
+                return res.status(400).json({
+                    status: 0,
+                    msg: 'Invalid ReceiverDetails'
+                });
+            }
+            let finalKafkaEventsPromise = [];
+            for (let i = 0; i < finalKafkaEvents.length; i++) {
+                finalKafkaEventsPromise.push(KafkaService.sendMessage('send-email-topic', JSON.stringify(finalKafkaEvents[i])));
+            }
+            let totalCnt = finalKafkaEventsPromise.length;
+            let rejectCnt = 0;
+            return Promise.allSettled(finalKafkaEventsPromise).then((responses) => {
+                responses.forEach((response, index) => {
+                    if (response.status == 'rejected') {
+                        rejectCnt++;
+                        return;
+                    }
+                });
+                return res.status(200).json({
+                    status: 1,
+                    msg: `${totalCnt - rejectCnt} out of ${totalCnt} events sent to kafka`
+                });
             });
         } catch (error) {
             console.log('Error in EmailController.sendMultipleEmailsKafka: ', error);
